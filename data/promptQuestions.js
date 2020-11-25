0329282f7099f0a65a5923c9c7221fe6af87bbec
joshuavaslie2021 <joshuavaslie2021@u.northwestern.edu>
2020-11-25 15:40:19 -0600

    async functionality added to dynamically fill out the full readme if user chooses to or not

---
+++ data/promptQuestions.js
@@ -0,0 +1,68 @@
+
+const inquirer = require('inquirer');
+        const startPrompt = [
+        {
+            type: 'input',
+            message: 'please enter developer(s) names',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'what is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'input year project was completed:',
+            name: 'year',
+        },
+        {
+            type: 'input',
+            message: 'enter your email address:',
+            name: 'email',
+        },
+        {
+            type: 'confirm',
+            message: 'were there other contributors on the project? y/n',
+            name: 'contributors',
+        
+        },
+        {
+            type: 'confirm',
+            message: 'complete READMe.md body?',
+            name: 'completeReadMe',
+        }
+    ];
+    const fullReadMe = [
+        {
+            type: 'input',
+            name: 'description',
+            message: `please provide a description for project?`
+        },
+        {
+            type:'input',
+            name:'installation',
+            message:'install info'
+        },
+        {
+            type: 'input',
+            message: 'provide link to github profile',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'describe the usage of the project',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'test input',
+            name: 'tests',
+        },
+        
+    ]
+    
+module.exports = {
+    questions:startPrompt,
+    extendedPrompts:fullReadMe
+}
