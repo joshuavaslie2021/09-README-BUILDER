db2d81911bda85a8f3dcec6014f82eda588b94de
Joshua Vaslie <joshuavaslie2021@u.northwestern.edu>
2020-11-26 20:17:34 -0600

    further code refactoring

--- index.js
+++ index.js
@@ -2,43 +2,44 @@
 const inquirer = require('inquirer');
 const { questions, extendedPrompts } = require('./data/promptQuestions');
 const { licenses } = require('./data/licenseScript');
-const genList = (licenses,userYear,userName) => {
-        return {
-            type:'rawlist',
-            message: 'select your choice of licensing',
-            name: 'license_choice',
-            choices: licenses,
-        }
-}  
+const getLicenseList = (licenses) => {
+    return {
+        type:'rawlist',
+        message: 'select your choice of licensing',
+        name: 'license_choice',
+        choices: licenses,
+    }
+};
 const main = async () => {
-    const startUpPrompts = await inquirer.prompt(questions)
-    const userYear = String(startUpPrompts.year)
-    const userName = String(startUpPrompts.name)
+    const startUpPrompts = await inquirer.prompt(questions);
+    
     if (startUpPrompts.completeReadMe) {
-    let writeCompleteReadme = await inquirer.prompt(extendedPrompts)
+        let writeCompleteReadme = await inquirer.prompt(extendedPrompts)
     }
     else{
-    var writeCompleteReadme = {
-        description: 'Sample Description to be updated later formally',
-        installation: 'Placeholder for Installation instructions if pending',
-        usage: 'sample',
-        tests: 'sample'
-    }
+        var writeCompleteReadme = {
+            description: 'Sample Description to be updated later formally',
+            installation: 'Placeholder for Installation instructions if pending',
+            usage: 'sample',
+            tests: 'sample'
+        }
     }
     
     // if (startUpPrompts.contributors) {
     // printReadMe(startUpPrompts,writeCompleteReadme)
     // }
-    const licensePrompt = await inquirer.prompt(genList(licenses,userName,userYear))
-    
-    printReadMe(startUpPrompts,writeCompleteReadme,licensePrompt)
+    const licensePrompt = await inquirer.prompt(getLicenseList(licenses,userName,userYear));
+    printReadMe(startUpPrompts,writeCompleteReadme,licensePrompt);
 }
 const printReadMe = (startUpPrompts,writeCompleteReadme,licensePrompt) => {
-let title = String(startUpPrompts.title).toUpperCase()
-let license = licensePrompt.license_choice
-let description = writeCompleteReadme.description
-let usage = writeCompleteReadme.usage
-let install = writeCompleteReadme.installation
+let title = String(startUpPrompts.title).toUpperCase();
+let license = licensePrompt.license_choice;
+let description = writeCompleteReadme.description;
+let usage = writeCompleteReadme.usage;
+let install = writeCompleteReadme.installation;
+var year = startUpPrompts.year;
+var name = startUpPrompts.name;
+this
 const fs = require('fs');
 const readMeText = 
 `## ${title}
@@ -59,5 +60,5 @@ ${usage}
  ${license}`
 fs.writeFile(`README.md`, readMeText, (err) =>
 err ? console.error(err) : console.log('Success!'))
-}
+};
 main()
